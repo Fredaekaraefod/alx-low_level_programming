3ea661f9073ab050f8564b8db9ce0efde50dc574
Fredaekaraefod <fredaefod@gmail.com>
2022-10-08 18:02:48 +0100

    Frsh commit

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+o#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints argc count
+ *@argc: argument count
+ *@argv: argument vector
+ * Return: 0 if succes
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argv;
+	printf("%d\n", argc - 1);
+	return (0);
+}
