490093ff3fc4b31faf0bfea99ef1753f2bc2a1f6
Fredaekaraefod <fredaefod@gmail.com>
2022-10-08 18:09:28 +0100

    Ff

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,16 +1,25 @@
-o#include "main.h"
+#include <stdlib.h>
 #include <stdio.h>
 
 /**
- * main - prints argc count
- *@argc: argument count
- *@argv: argument vector
- * Return: 0 if succes
- */
+ * main - a program that prints the number of arguments
+ *        passed into it
+ *
+ * @argc: holds the number of arguments passed
+ * @argv: array pointer that holds the arguments passed
+ *
+ * Return: Always 0 (Success)
+*/
 
-int main(int argc, char *argv[])
+int main(int argc, char __attribute__((unused)) *argv[])
 {
-	(void) argv;
-	printf("%d\n", argc - 1);
+	int i = 0, m;
+
+	while (i < argc)
+	{
+		m = i;
+		i++;
+	}
+	printf("%d\n", m);
 	return (0);
 }
